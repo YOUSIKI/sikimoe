190f0479e85479f530589595cadcf27aacab4590
YouSiki <you.siki@outlook.com>
2025-01-05 02:38:48 +0800

    feat: update ESLint configuration to include JavaScript support and enhance Tailwind CSS configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,28 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import js from "@eslint/js";
 
 export default [
   {
     ignores: [".astro/**/*", "dist/**/*", "node_modules/**/*"],
   },
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        setTimeout: "readonly",
+        window: "readonly",
+        navigator: "readonly",
+        localStorage: "readonly",
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
   {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
