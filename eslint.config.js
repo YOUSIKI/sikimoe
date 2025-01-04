11f1818cc8407a34265372512b41c52b13e8a80d
YouSiki <you.siki@outlook.com>
2025-01-05 02:27:07 +0800

    chore: simplify ESLint configuration by removing Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,3 @@
 import eslintPluginAstro from "eslint-plugin-astro";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
 
-export default [
-  ...eslintPluginAstro.configs.recommended,
-  eslintConfigPrettier,
-  {
-    plugins: {
-      prettier: eslintPluginPrettier,
-    },
-    rules: {
-      "prettier/prettier": "error",
-    },
-  },
-];
+export default [...eslintPluginAstro.configs.recommended];
