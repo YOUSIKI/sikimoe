57c0c9801fb257bbe9c145b38ceb46a9f1092ca5
YouSiki <you.siki@outlook.com>
2025-01-05 02:33:36 +0800

    feat: enhance ESLint configuration with TypeScript support and custom formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,27 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
-export default [...eslintPluginAstro.configs.recommended];
+export default [
+  {
+    ignores: [".astro/**/*", "dist/**/*", "node_modules/**/*"],
+  },
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
